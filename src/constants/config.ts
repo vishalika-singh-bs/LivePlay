d48418c77d2251221b89de84fa81b573b8073d6a
Vishalika Singh <vishalika.singh@bigsteptech.com>
2025-04-30 23:26:27 +0530

    add env file

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,22 +1,21 @@
 export type StreamingType = "AMAZON_LATENCY" | "AMAZON_REALTIME" | "AGORA";
 
-export const STREAMING_TYPE: StreamingType = "AMAZON_LATENCY"; // Change as needed
+export const STREAMING_TYPE: StreamingType = import.meta.env.VITE_STREAMING_TYPE as StreamingType;
 
 // Agora Streaming
 export const AGORA_STREAMING = {
-    defaultVideoUrl: 'https://lpm-public-files.s3.us-west-2.amazonaws.com/Haley+on+LPB+BG+with+Audio+(1).mp4',
+    defaultVideoUrl: import.meta.env.VITE_AGORA_DEFAULT_VIDEO_URL!,
 };
 
 // Amazon IVS (Latency Streaming)
 export const AMAZON_LATENCY_STREAMING = {
-    playbackUrl: "https://37c9445dfd56.us-west-2.playback.live-video.net/api/video/v1/us-west-2.926856021716.channel.WNsjoC3d16Sj.m3u8",
+    playbackUrl: import.meta.env.VITE_IVS_LATENCY_PLAYBACK_URL!,
 };
 
 // Amazon IVS Real-Time Streaming
-//   export const AMAZON_REALTIME_STREAMING = {
-//     stageArn: "arn:aws:ivs:us-west-2:your-account-id:stage/your-stage-id", // Required for real-time stages
-//     ingestEndpoint: "wss://your-stage-endpoint.ivs.us-west-2.amazonaws.com", // WebSocket endpoint for participant connection
-//     participantToken: "your-generated-token", // Generated via AWS SDK or backend
-//     channelName: "RealtimeStreamingStage",
-//   };
-
+export const AMAZON_REALTIME_STREAMING = {
+    stageArn: import.meta.env.VITE_IVS_STAGE_ARN!,
+    ingestEndpoint: import.meta.env.VITE_IVS_INGEST_ENDPOINT!,
+    participantToken: import.meta.env.VITE_IVS_PARTICIPANT_TOKEN!,
+    channelName: import.meta.env.VITE_IVS_CHANNEL_NAME!,
+};
